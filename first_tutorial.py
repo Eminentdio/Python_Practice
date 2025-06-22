cf0fb6e7dfe838cb7486028eb5e80aa1463ddafc
Ismail O. Daud <eminentdio@gmail.com>
2025-06-22 01:21:24 -0700

    Add files via upload

---
+++ first_tutorial.py
@@ -0,0 +1,26 @@
+# # Conditional Statement With Practice
+#
+# def favourite_colour_selector(): # A function was created for the selection
+#
+#     name = input("Enter your name: ") # The user was asked to enter his or her name
+#
+#     sex = input("Are you a male or a female?: ") # The user gender was collected
+#
+#     if sex == "male": # It was checked if the user is a male
+#
+#         favourite = input("Do you like a blue trouser? Y/N: ") # An input was demanded to choose the user's colour
+#
+#         if favourite == "Y": # It was checked if the user like the colour or not.
+#
+#             print(f'Your name is {name} and you like a blue trouser')
+#         else:
+#             print(f"Your name is {name} and you don't like a blue trouser")
+#
+#     elif sex != "male":
+#         favourite = input("Do you like a pink skirt? Y/N: ")
+#         if favourite == "Y":
+#             print(f'Your name is {name} and you like a pink trouser')
+#         else:
+#             print(f"Your name is {name} and you don't like a pink trouser")
+#
+# favourite_colour_selector() #The function was call again.
---
+++ name_for_gender_dectector.py
@@ -0,0 +1,47 @@
+# Create a dictionary of gendered names
+# Design a function for gender_detector
+# Create a variable to collect the user's name
+# Check for the gender of the user with name entered
+# Print the result for the user to see
+# If the user's name is gender, prompt a variable that asks for his gender
+# Print that the user's gender has been saved for future usage
+
+gendered_names = {
+    "Adenike" : "female",
+    "Olawumi" : "female",
+    "Adewale" : "male",
+    "Bunmi"   : "female",
+    "Chris"   : "male",
+    "Funmi"   : "female",
+    "Yusuff"  : "male",
+    "Dare"    : "male",
+    "Tunde"   : "male",
+    "Yetunde" : "female",
+    "Ismail"  : "male",
+    "Iyabo"   : "female",
+    "Akande"  : "male",
+    "Asake"   : "female",
+    "Ayoka"   : "female",
+    "Akanni"  : "male"
+}
+
+def gender_detector():
+    print("Welcome to Gender Detector Page")
+    user_name = input("Enter your name: ")
+
+    for key, value in gendered_names.items():
+
+        if user_name == key:
+            print(f"Welcome, {user_name}! You are a {value} 😘💕💖😜😍")
+
+
+        elif user_name not in gendered_names.keys():
+            user_gender = input("Enter your gender: ")
+
+            if user_gender.lower() not in gendered_names.values():
+                print(f"Hey, {user_name}! I don't understand your gender. Enter a correct gender")
+            else:
+                print(f"Thanks, {user_name}. Your gender is {user_gender}")
+                return
+
+gender_detector()
